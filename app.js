2e65816b15aae0fb173f3e76a3ea77233abeb10d
HINA IRFAN <160302309+hinairfan4@users.noreply.github.com>
2024-05-28 09:48:01 +0500

    add files

---
+++ app.js
@@ -0,0 +1,32 @@
+
+var variable1 = 1, variable2 = 2, variable3 = 3;
+
+// 2. Declare 5 legal & 5 illegal variable names.
+
+// legal variable
+var myName;
+var my_Name;
+var $myName;
+var myName123;
+var MyName;
+
+// illegal variable
+// var 1myName;
+// var my-Name;
+// var my Name;
+// var !myName;
+// var var;
+
+// 3. Display this in your browser
+// a) A heading stating “Rules for naming JS variables”
+// b) Variable names can only contain ______, ______,
+// ______ and ______.
+// For example $my_1stVariable
+// c) Variables must begin with a ______, ______ or
+// _____. For example $name,
+
+document.write("Rules for naming JS variables <br>");
+document.write("Variable names can only contain number, underscore dollar sign and letter");
+
+
+
